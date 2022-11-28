a2d4fbee5705fa30d510d96c75ab6c1c0121bf63
Andreas Voss <andreas@anvo.dk>
2022-11-28 06:00:25 +0100

    Added VPN to installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -44,8 +44,11 @@ sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedora-
 # Install zsh-completions
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/Fedora_36/shells:zsh-users:zsh-completions.repo
 
+# VPN
+sudo dnf copr -y enable yuezk/globalprotect-openconnect
+
 # Install from default repos
-sudo dnf install -y neovim dotnet-sdk-6.0 nodejs ulauncher gnome-shell-extension-appindicator jq wmctrl google-cloud-cli gnome-shell-extension-dash-to-dock papirus-icon-theme azure-cli enpass gtk-murrine-engine gtk2-engines helm pgadmin4 ffmpeg-libs discord tilix libgtop2-devel lm_sensors gnome-extensions-app gnome-tweaks remmina remmina-plugins-rdp zsh util-linux-user zsh-completions bat ripgrep tree-sitter-cli libstdc++-static libstdc++ gcc-c++ rust cargo
+sudo dnf install -y neovim dotnet-sdk-6.0 nodejs ulauncher gnome-shell-extension-appindicator jq wmctrl google-cloud-cli gnome-shell-extension-dash-to-dock papirus-icon-theme azure-cli enpass gtk-murrine-engine gtk2-engines helm pgadmin4 ffmpeg-libs discord tilix libgtop2-devel lm_sensors gnome-extensions-app gnome-tweaks remmina remmina-plugins-rdp zsh util-linux-user zsh-completions bat ripgrep tree-sitter-cli libstdc++-static libstdc++ gcc-c++ rust cargo globalprotect-openconnect
 
 # Install flatpaks
 sudo flatpak install --noninteractive --assumeyes com.slack.Slack com.microsoft.Teams com.spotify.Client com.getpostman.Postman
